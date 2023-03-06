938feb051ae4f6286ccf3a3e20e0323e5cd7eda3
shikayo <japanlover1337@gmail.com>
2023-03-06 19:42:43 +0300

    init

---
+++ KpopMerchAPI/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KpopMerchAPI.Controllers;
+
+public class HomeController : ControllerBase
+{
+    public IActionResult Index()
+    {
+        return null;
+    }
+}
